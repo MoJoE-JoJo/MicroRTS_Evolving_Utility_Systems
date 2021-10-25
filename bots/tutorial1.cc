e15b0ae54e9bb51ea5fb745ad1da7509a4f337ea
Jonathan Emil Mogensen <jmog@itu.dk>
2021-10-25 10:44:50 +0200

    Extra map

--- bots/tutorial1.cc
+++ bots/tutorial1.cc
@@ -9,64 +9,6 @@
 #include "bot_examples.h"
 
 
-/*
-using namespace sc2;
-
-class Bot : public Agent
-{
-public:
-    virtual void OnGameStart() final
-    {
-        std::cout << "Hello, World!" << std::endl;
-    }
-
-    virtual void OnStep() final
-    {
-        std::cout << Observation()->GetGameLoop() << std::endl;
-        std::cout << Observation()->GetUnits(sc2::Unit::Alliance::Self).size() << std::endl;
-    }
-
-    virtual void OnGameEnd() final
-    {
-        std::cout << Observation()->GetResults()[0].player_id;
-        std::cout << Observation()->GetResults()[0].result;
-        std::cout << "\n";
-        std::cout << Observation()->GetResults()[1].player_id;
-        std::cout << Observation()->GetResults()[1].result;
-        std::cout << "\n";
-    }
-
-};
-
-int main(int argc, char* argv[])
-{
-    Coordinator coordinator;
-    coordinator.LoadSettings(argc, argv);
-    //sc2::RenderSettings settings(10,10,0,0);
-    coordinator.SetRealtime(true);
-    //coordinator.SetRender(settings);
-    coordinator.SetStepSize(100);
-    Bot bot;
-    coordinator.SetParticipants({
-    CreateParticipant(Race::Terran, &bot),
-    CreateComputer(Race::Zerg)
-        });
-    coordinator.LaunchStarcraft();
-    int gamesCounter = 0;
-
-    while (gamesCounter < 2)
-    {
-        gamesCounter++;
-        coordinator.StartGame(sc2::kMapBelShirVestigeLE);
-        while (coordinator.Update())
-        {
-        }
-    }
-    return 0;
-}
-*/
-
-
 
 //*************************************************************************************************
 int main(int argc, char* argv[])
@@ -89,7 +31,7 @@ int main(int argc, char* argv[])
 
     // Start the game.
     coordinator.LaunchStarcraft();
-    const char* newMap = "Example/TestMap1.SC2Map";
+    const char* newMap = "Ladder/TestModified.SC2Map";
     coordinator.StartGame(newMap);
 
     while (coordinator.Update())
