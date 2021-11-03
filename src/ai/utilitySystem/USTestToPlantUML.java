7f356e3db51519718517701240fc3d004d5b9de9
Gebuz <6773230+Gebuz@users.noreply.github.com>
2021-11-03 15:19:04 +0100

    add a c

--- src/ai/utilitySystem/USTestToPlantUML.java
+++ src/ai/utilitySystem/USTestToPlantUML.java
@@ -22,17 +22,26 @@ public class USTestToPlantUML {
         USVariable v3 = new USVariable("v3", GameStateVariables.PLAYER_WARRIORS);
         variables.add(v3);
 
+        // constants
+        USConstant c1 = new USConstant(0.1f);
+
         // features
         USFeature f1 = new USFeature("f1", Operation.DIVIDE, v1, v2);
         features.add(f1);
         USFeature f2 = new USFeature("f2", Operation.MULTIPLY, v2, v3);
         features.add(f2);
+        USFeature f3 = new USFeature("f3", Operation.MULTIPLY, v3, c1);
+        features.add(f3);
+        USFeature f4 = new USFeature("f4", Operation.MULTIPLY, f2, f3);
+        features.add(f4);
 
         // actions
         USAction a1 = new USAction("a1", f1, new PlayerAction());
         actions.add(a1);
         USAction a2 = new USAction("a2", f2, new PlayerAction());
         actions.add(a2);
+        USAction a3 = new USAction("a3", f4, new PlayerAction());
+        actions.add(a3);
 
         UtilitySystem us = new UtilitySystem(variables, features, actions);
         String plantUML = us.toPlantUML();
