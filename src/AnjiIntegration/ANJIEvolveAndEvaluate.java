0bff5879afb45452390997b7d01c693a479dc6b4
Jonathan Emil Mogensen <jmog@itu.dk>
2021-12-08 18:57:36 +0100

    Fixed test12 and test 11, I think

--- src/AnjiIntegration/ANJIEvolveAndEvaluate.java
+++ src/AnjiIntegration/ANJIEvolveAndEvaluate.java
@@ -16,7 +16,7 @@ public class ANJIEvolveAndEvaluate {
 
     public static void main(String[] args) {
 
-        String array[] = {"1", "2", "3", "4", "5", "11", "12", "13"};
+        String array[] = {/*"1", "2", "3", "4", "5", "9"*/ "11", "12"};
 
         for (String s : array) {
             try {
