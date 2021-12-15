607ebb4f192a21302cb0969d4da9a224c6a832ec
Marcus <noller02@hotmail.com>
2021-12-15 14:17:06 +0100

    ant build file and runscripts, modified script

---
+++ runScripts/README.txt
@@ -0,0 +1,20 @@
+Instructions for running the compiled jar file:
+
+Included is 3 scripts
+
+- startFrontend.bat: boots up the GUI window to play around with microRTS
+- startQuicktest.bat: runs a mini evolution from baseline against baseline to see if everything works.
+- startAllTests: runs all the tests described in the paper.
+
+
+if you want to run with your own config, it can be done with the following command:
+
+    'java -cp MicroRTS_EUS.jar properties\utility_system_properties\quickTest.properties'
+
+Where the quickTest.properties is then replaced with the properties file you want to run.
+Either modify one of the script files or run it form a terminal
+
+
+
+
+
--- src/AnjiIntegration/ANJIEvolveAndEvaluate.java
+++ src/AnjiIntegration/ANJIEvolveAndEvaluate.java
@@ -5,6 +5,7 @@ import com.anji.neat.Evolver;
 import com.anji.util.Properties;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -14,70 +15,79 @@ import java.nio.file.StandardCopyOption;
 
 public class ANJIEvolveAndEvaluate {
 
-    public static void main(String[] args) {
-
-        String array[] = {/*"1", "2", "3", "4", "5", "11",*/ "9"};
-
-        for (String s : array) {
-            try {
-
-                // === RUN EVOLUTION ===
-                Properties props = new Properties("utility_system_properties/test" + s + ".properties");
-                //Properties props = new Properties(args[0]);
-                Evolver evolver = new Evolver();
-                evolver.init(props);
-                evolver.run();
-
-                //sleep 3 sec to ensure the .db files gets saved to disk
-                Thread.sleep(3000);
-
-                System.out.println("Working Directory = " + System.getProperty("user.dir"));
-                // after evolving is done, save backups of the results in another folder
-
-                // create file structure: storage/{prop.name}
-                String runName = props.getProperty("run.name");
-                String filepath = "./storage/" + runName;
-                new File(filepath).mkdirs();
-
-                // save chromosome as XML
-                String chromSrc = "./db/chromosome/chromosome" + evolver.getChamp().getId() + ".xml";
-
-                Path src = Paths.get(chromSrc);
-                Path dst = Paths.get(filepath + "/" + runName + "_ChampionChromosome.xml");
-
-                Files.copy(src, dst, StandardCopyOption.REPLACE_EXISTING);
-
-                // copy and rename fitness XML file
-                String fitnessFileSrc = "./nevt/fitness/fitness.xml";
-
-                Path fitnessSrc = Paths.get(fitnessFileSrc);
-                Path fitnessDst = Paths.get(filepath + "/" + runName + "_fitness.xml");
-
-                Files.copy(fitnessSrc, fitnessDst, StandardCopyOption.REPLACE_EXISTING);
-
-                // create UML of resulting utility system and save in a txt.
-                anjiConverter conv = new anjiConverter();
-                UtilitySystem utilSystem = conv.toUtilitySystemFromChromosome(evolver.getChamp().getId());
-
-                String uml = utilSystem.toPlantUML();
-
-                Path umlPath = Paths.get(filepath + "/" + runName + "_uml.txt");
-
-                try {
-                    Files.writeString(umlPath, uml, StandardCharsets.UTF_8);
-                } catch (IOException ex) {
-                    // Handle exception
-                }
-
-                // EVALUATE
-
-                EvaluateChromosome.eval(props, utilSystem);
-
-            } catch (Exception e) {
-                e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            String array[] = {"1", "2", "3", "4", "5", "9", "11",};
+            for (String s : array) {
+                run(new Properties("utility_system_properties/test" + s + ".properties"));
             }
         }
+        else
+        {
+            System.out.println(System.getProperty("user.dir"));
+            System.out.println(args[0]);
+            Properties props = new Properties();
+            props.load(new FileInputStream(args[0]));
+            run(props);
+        }
         System.exit(0);
+    }
+
+    private static void run(Properties props) {
+        try {
+
+            // === RUN EVOLUTION ===
+            Evolver evolver = new Evolver();
+            evolver.init(props);
+            evolver.run();
+
+            //sleep 3 sec to ensure the .db files gets saved to disk
+            Thread.sleep(3000);
+
+            System.out.println("Working Directory = " + System.getProperty("user.dir"));
+            // after evolving is done, save backups of the results in another folder
+
+            // create file structure: storage/{prop.name}
+            String runName = props.getProperty("run.name");
+            String filepath = "./storage/" + runName;
+            new File(filepath).mkdirs();
+
+            // save chromosome as XML
+            String chromSrc = "./db/chromosome/chromosome" + evolver.getChamp().getId() + ".xml";
+
+            Path src = Paths.get(chromSrc);
+            Path dst = Paths.get(filepath + "/" + runName + "_ChampionChromosome.xml");
+
+            Files.copy(src, dst, StandardCopyOption.REPLACE_EXISTING);
+
+            // copy and rename fitness XML file
+            String fitnessFileSrc = "./nevt/fitness/fitness.xml";
+
+            Path fitnessSrc = Paths.get(fitnessFileSrc);
+            Path fitnessDst = Paths.get(filepath + "/" + runName + "_fitness.xml");
+
+            Files.copy(fitnessSrc, fitnessDst, StandardCopyOption.REPLACE_EXISTING);
+
+            // create UML of resulting utility system and save in a txt.
+            anjiConverter conv = new anjiConverter();
+            UtilitySystem utilSystem = conv.toUtilitySystemFromChromosome(evolver.getChamp().getId());
+
+            String uml = utilSystem.toPlantUML();
+
+            Path umlPath = Paths.get(filepath + "/" + runName + "_uml.txt");
+
+            try {
+                Files.writeString(umlPath, uml, StandardCharsets.UTF_8);
+            } catch (IOException ex) {
+                // Handle exception
+            }
+
+            // evaluate the champion utility system.
+            EvaluateChromosome.eval(props, utilSystem);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
